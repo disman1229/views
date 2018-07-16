3aa770de2ae4e21d2fcfb0c46e372f78708395b9
disman1229 <346025232@qq.com>
2018-07-16 22:45:54 +0800

    重定向

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,13 @@ urlpatterns = [
     path('',views.index,name="index"),
     path('attribles/',views.attribles),
     path('get1/',views.get1),
-    path('get2/',views.get2)
+    path('get2/',views.get2),
+    path('showregist/',views.showregist),
+    path('showregist/regist/',views.regist),
+    # path('showresponse/',views.showresponse)
+    path('cookietest/',views.cookietest),
+    path('redirect1/',views.redirect1),
+    path('redirect2/',views.redirect2),
+
+
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 
 def index(request):
 
@@ -20,7 +20,53 @@ def attribles(request):
 def get1(request):
     a = request.GET.get("a")
     b = request.GET.get("b")
+    c = request.GET.get("c")
+    return HttpResponse( a +" "+ b +" "+ c )
 
 
 def get2(request):
-    pass
+    a = request.GET.getlist('a')
+    a1 = a[0]
+    a2 = a[1]
+    c = request.GET.get("c")
+    return HttpResponse(a1 + " " + a2 + " " + c)
+
+#POST
+def showregist(request):
+    return render(request,'myapp/regist.html')
+def regist(request):
+    name = request.POST.get("name")
+    gender = request.POST.get("gender")
+    age = request.POST.get("age")
+    hobby = request.POST.getlist("hobby")
+    print(name)
+    print(gender)
+    print(age)
+    print(hobby)
+    return HttpResponse("12312")
+
+# #showresponse
+# def showresponse(request):
+#     res = HttpResponse()
+#     res.content = b'good'
+#     print(res.content)
+#     print(res.charset)
+#     print(res.status_code)
+#     print(res.content-type)
+#     return res
+
+
+def cookietest(request):
+    res = HttpResponse()
+    cookie = request.COOKIES
+    res.write("<h1>"+cookie["disman"]+"<h1/>")
+    # cookie = res.set_cookie("disman","good")
+    return res
+
+
+#重定向
+def redirect1(request):
+    # return HttpResponseRedirect('/redirect2')
+    return redirect('/redirect2')
+def redirect2(request):
+    return HttpResponse("我是重定向视图")
